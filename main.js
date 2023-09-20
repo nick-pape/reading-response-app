9fc77a89ba36ef1ae0f1f26a1abdd49b3fcd8521
Nick Pape <5674316+nick-pape@users.noreply.github.com>
2023-09-19 20:31:01 -0500

    Add app resources

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.loadFile('index.html')
+}
+
+// Close the app when all windows closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+
+app.whenReady().then(() => {
+    createWindow();
+    
+    // MacOS doesn't close windows, so reopen if there isn't an existing one
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
